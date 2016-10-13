de300e314ba84a2ac751eb1c996a66aefc856e2c
Viktoria Maximova <maximova14@gmail.com>
2016-10-13 23:02:06 +0300

    First commit

---
+++ app/src/main/java/com/example/client/MenuActivity.java
@@ -0,0 +1,28 @@
+package com.example.client;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Button petuhButton = (Button) findViewById(R.id.PetuhButton);
+        petuhButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello pidor", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(MenuActivity.this, MenuActivity.class);
+//                startActivity(intent);
+//                attemptLogin();
+            }
+        });
+    }
+}
