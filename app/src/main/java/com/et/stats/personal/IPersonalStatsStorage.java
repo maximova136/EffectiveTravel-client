926e572f93b18545afb314984fbe7829591af42e
Vlad Markin <vlad.d.markin@gmail.com>
2016-12-07 23:27:24 +0300

    PersonalStats storage ready and covered with tests

--- app/src/main/java/com/et/stats/personal/IPersonalStatsStorage.java
+++ app/src/main/java/com/et/stats/personal/IPersonalStatsStorage.java
@@ -4,6 +4,6 @@ package com.et.stats.personal;
 import java.util.HashMap;
 
 public interface IPersonalStatsStorage {
-    public boolean save();
+    public boolean save(HashMap<String, Integer> personalStats);
     public HashMap<String, Integer> load();
 }
--- app/src/main/java/com/et/stats/personal/PersonalStatsStorage.java
+++ app/src/main/java/com/et/stats/personal/PersonalStatsStorage.java
@@ -1,11 +1,17 @@
 package com.et.stats.personal;
 
 
+import com.et.exception.storage.DeleteObjectFailed;
+import com.et.exception.storage.LoadCollectionFailed;
+import com.et.exception.storage.PutObjectFailed;
 import com.et.storage.ILocalStorage;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PersonalStatsStorage implements IPersonalStatsStorage {
+    public static final String COLLECTION_NAME = "personal_stats";
 
     private ILocalStorage storage;
 
@@ -13,14 +19,50 @@ public class PersonalStatsStorage implements IPersonalStatsStorage {
         this.storage = localStorage;
     }
 
-
     @Override
-    public boolean save() {
-        return false;
+    public boolean save(HashMap<String, Integer> personalStats) {
+        try {
+            storage.clearCollection(COLLECTION_NAME);
+
+            HashMap<String, String> item = new HashMap<>();
+            for(Map.Entry<String, Integer> pair : personalStats.entrySet() ) {
+                item.put(pair.getKey(), pair.getValue().toString());
+            }
+
+            storage.putObject(COLLECTION_NAME, item);
+
+            return true;
+        }
+        catch (DeleteObjectFailed e) {
+            return false;
+        }
+        catch (PutObjectFailed e) {
+            return false;
+        }
     }
 
     @Override
     public HashMap<String, Integer> load() {
-        return null;
+        try {
+            HashMap<String, Integer> loadedStats = new HashMap<>();
+            List< HashMap<String, String> > collection = storage.loadCollection(COLLECTION_NAME);
+
+            if(collection == null || collection.size() <= 0)
+                return null;
+
+            HashMap<String, String> item = collection.get(0);
+
+            for(Map.Entry<String, String> pair : item.entrySet() ) {
+                loadedStats.put(pair.getKey(), Integer.parseInt(pair.getValue()));
+            }
+
+            return loadedStats;
+        }
+        catch (NumberFormatException e) {
+            return null;
+        }
+        catch (LoadCollectionFailed e) {
+            return null;
+        }
     }
 }
